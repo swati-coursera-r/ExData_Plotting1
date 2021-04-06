085d9a72698e17c64ae79ae49698e96b9af72a53
Swati <swatgupta375@gmail.com>
2021-04-06 23:50:56 +0530

    plot 2 code uploaded

--- plot2.R
+++ plot2.R
@@ -5,8 +5,10 @@ data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec="
 ##selecting formentioned date
 dateddata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#plotting histogram
+##plotting plot2
+
+datetime <- strptime(paste(dateddata$Date, dateddata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globeactivepower <- as.numeric(dateddata$Global_active_power)
-png("plot1.png")
-hist(globeactivepower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+png("plot2.png")
+plot(datetime, globeactivepower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
